e1543f33d88132e457f229df5c3cc822520e8da8
andrealemonroy <andreale17@icloud.com>
2018-10-30 17:25:23 -0500

    sidenav

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,21 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
 new Chart(document.getElementById("inversorPie"), {
-    type: 'pie',
-    data: {
-      labels: ["Caja y equivalentes", "Renta Fija", "Renta Variable", "Renta Alternativa"],
-      datasets: [{
-        // label: "tipo de inversor",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
-        data: [35, 12,5,48]
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Tipo de inversión'
-      }
+  type: 'pie',
+  data: {
+    labels: ["Caja y equivalentes", "Renta Fija", "Renta Variable", "Renta Alternativa"],
+    datasets: [{
+      // label: "tipo de inversor",
+      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
+      data: [35, 12, 5, 48]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Tipo de inversión'
     }
+  }
 });
