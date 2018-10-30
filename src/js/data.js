040e036d1c380272b3083e7febfdb9f61e621120
vmirella <vmirella_17@hotmail.com>
2018-10-30 17:40:09 -0500

    Configurando firebase y subiendo preguntas

---
+++ src/js/data.js
@@ -0,0 +1,12 @@
+/* const newPostKey = firebase.database().ref().child('questions').push().key;
+firebase.database().ref('questions/'+ newPostKey).set({
+  text: '8. Las crisis financieras no son el estado habitual del mercado. Sin embargo, considerando el retorno anual promedio y la máxima caída que se registraría en una crisis, ¿cuál de las siguientes opciones te parece la más adecuada?',
+  options: {
+    respuesta1: 'Portafolio A',
+    respuesta2: 'Portafolio B',
+    respuesta3: 'Portafolio C',
+    respuesta4: 'Portafolio D',
+    respuesta5: 'Portafolio E'
+  },
+  image: ''
+}); */
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAWhYg7ooZsREWBlcDS4yCrKUOh2tQhVVk",
+  authDomain: "inteligo-hackaton.firebaseapp.com",
+  databaseURL: "https://inteligo-hackaton.firebaseio.com",
+  projectId: "inteligo-hackaton",
+  storageBucket: "inteligo-hackaton.appspot.com",
+  messagingSenderId: "212977010921"
+};
+firebase.initializeApp(config);
+let database = firebase.database();
+
+
 new Chart(document.getElementById("inversorPie"), {
     type: 'pie',
     data: {
