f654a7e131a718972dd9eff016d74f00ee96f45c
KaluGN <kaluguzmann@gmail.com>
2018-10-31 00:55:40 -0500

    funcionalidad para el registro OK

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-
-// Initialize Firebase
-const config = {
-  apiKey: "AIzaSyAWhYg7ooZsREWBlcDS4yCrKUOh2tQhVVk",
-  authDomain: "inteligo-hackaton.firebaseapp.com",
-  databaseURL: "https://inteligo-hackaton.firebaseio.com",
-  projectId: "inteligo-hackaton",
-  storageBucket: "inteligo-hackaton.appspot.com",
-  messagingSenderId: "212977010921"
-};
-firebase.initializeApp(config);
 let database = firebase.database();
 
 $(document).ready(function(){
